23043e13fe2dcff230565bab5c6eeb9a9c444d47
ren1244 <n1244506804@hotmail.com>
2021-04-27 14:32:12 +0800

    add sfmk\Request and fix htaccess

--- sfmk/Request.php
+++ sfmk/Request.php
@@ -4,6 +4,105 @@ namespace sfmk;
 
 class Request
 {
+    const FILTER_MAP=[
+        'string' => FILTER_DEFAULT,
+        'int' => FILTER_VALIDATE_INT,
+        'bool' => FILTER_VALIDATE_BOOLEAN,
+        'float' => FILTER_VALIDATE_FLOAT,
+    ];
+    private $classname;
+    private $refClass;
+
+    /**
+     * 建構
+     * 
+     * @param string $calssname 過濾類別名稱，此類別定義了參數的過濾規則
+     * @return void
+     */
+    public function __construct(string $classname)
+    {
+        $this->classname=$classname;
+        $this->refClass=new \ReflectionClass($classname);
+    }
+
+    /**
+     * 取得參數的資料 
+     * 
+     * @param int $inputType 類型 INPUT_GET, INPUT_POST, INPUT_COOKIE, INPUT_SERVER, or INPUT_ENV
+     * @param key $key 參數名稱
+     * @param bool $optional 這個參數是否為可選的
+     * @return mixed|null 過濾成功會回傳此參數的資料，否則交給「過濾類別」的 badRequest 方法處理
+     *                    若無此參數時，會依據 $optional 參數回傳 null 或是交給「過濾類別」的 badRequest 方法處理
+     */
+    public function getData($inputType, $key, $optional=false)
+    {
+        //取得 type 
+        if(!method_exists($this->classname, $key)) {
+            $this->badRequest();
+        }
+        $refMethod=$this->refClass->getMethod($key);
+        $refParams=$refMethod->getParameters();
+        $type=$refParams[0]->getType();
+        $type=$type!==null?$type->getName():'string';
+        //依據類型，過濾參數
+        $val=filter_input($inputType, $key, self::FILTER_MAP[$type]);
+        if($val===null && !$optional) { //required but not set
+            $this->badRequest();
+        }
+        if($type==='bool') {
+            if($val===null) {
+                return null;
+            }
+            $val=filter_input($inputType, $key, self::FILTER_MAP[$type], FILTER_NULL_ON_FAILURE);
+        }
+        if(
+            ($type==='bool'?$val===null:$val===false) ||
+            ($val!==null && $refMethod->invoke(null, $val)===false)
+        ) { //filter failure
+            $this->badRequest();
+        }
+        return $val;
+    }
+
+    /**
+     * 取得多個參數，以 [ker=>val,...] 方式回傳
+     * 
+     * @param int $inputType 同 getData
+     * @param array $params 這個陣列有兩個 key，分別為 'required' 與 'optional'
+     *              'required' 與 'optional' 的值為一個陣列
+     *              此陣列列出了要被取得參數的名稱
+     * @return array [參數名稱=>值] 的關聯陣列
+     */
+    public function getArray($inputType, $params=[])
+    {
+        $result=[];
+        if(isset($params['required'])) {
+            foreach($params['required'] as $key) {
+                $result[$key]=$this->getData($inputType, $key, false);
+            }
+        }
+        if(isset($params['optional'])) {
+            foreach($params['optional'] as $key) {
+                $result[$key]=$this->getData($inputType, $key, true);
+            }
+        }
+        return $result;
+    }
+
+    /**
+     * 當錯誤發生時的處理
+     */
+    private function badRequest()
+    {
+        if(method_exists($this->classname, 'onBadRequest')) {
+            call_user_func([$this->classname, 'onBadRequest']);
+        } else {
+            http_response_code(400);
+            echo 'Bad Request';
+            exit();
+        }
+    }
+
     /**
      * 類似 filter_input_array 取得過濾後的輸入值
      * https://www.php.net/manual/en/function.filter-input-array.php
